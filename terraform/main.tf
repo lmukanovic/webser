245db240a33fca77f8c5d44e0b7914a22fb70828
Lamia Mukanovic <lmukanovic@gmail.com>
2018-04-04 19:48:29 -0400

    first change to main file to get terraform working

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,5 +7,33 @@ terraform {
  }
 }
 provider "google" {
+  credentials = ""
+  project     = "comp698-lm2020"
   region = "us-central1"
+}
+//instances
+resource "google_compute_instance" "webser"{
+	name = "terraform-webservers"
+	description = "Terraform test instance group-1"
+	machine_type = " "
+	zone = "us-central1"
+
+	
+
+	network_interface {
+	network = "default"
+	access_config {
+		//104.197.78.154 ?Empheral IP
+    	}
+	}
+
+	count = 1 
+	lifecyle = {
+		create_before_destroy = true
+	}
+
+	instances = [
+    "${google_compute_instance.test.self_link}",
+    
+  ]
 }
