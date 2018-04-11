3a55c6b865388072c06349ebc0764f6ba43f1c29
Lamia Mukanovic <lmukanovic@gmail.com>
2018-04-11 19:25:49 -0400

    first try today if this new file will work

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,41 @@
 terraform {
- backend "gcs" {
-   project = "comp698-lm2020"
-   bucket  = "comp698-lm2020-terraform-state"
-   prefix  = "terraform-state"
+backend "gcs" {
+	project = "comp698-lm2020"
+	bucket  = "comp698-lm2020-terraform-state"
+	prefix  = "terraform-state"
  }
 }
+
 provider "google" {
-  credentials = ""
   project     = "comp698-lm2020"
   region = "us-central1"
 }
+
+
 //instances
-resource "google_compute_instance" "webser"{
-        name = "terraform-webservers"
-        description = "Terraform test instance group-1"
-        machine_type = " "
-        zone = "us-central1"
+resource "google_compute_instance_template" "webser"{
+        name = "terraform-webserver"
 
-
-
-        network_interface {
-        network = "default"
-        access_config {
-                104.197.78.154
-                // ?Empheral IP
         }
+
+        
+        machine_type         = "f1-mico"
+
+        disk {
+        source_image = "cos-cloud/cos-stable"
+        }
+
+
+  	network_interface {
+ 	network = "default"
+  }
+
+       
 }
-
-        count = 1
-        lifecyle = {
-                create_before_destroy = true
-        }
-
-        //instances = [
-   // "${google_compute_instance.test.self_link}",
-
- // ]
+resource "google_compute_instance_group_manager" instance_group_manager" 	{
+name               = "instance-group-manager"
+instance_template  = "${google_compute_instance_template instance_template.self_link}"
+base_instance_name = "instance-group-manager"
+zone               = "us-central1-f"
+target_size        = "1"
 }
